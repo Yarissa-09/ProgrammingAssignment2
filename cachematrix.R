8f518b6e31f4ff47ac7ab181df2af8fc0702ee33
Yarissa-09 <77834653+Yarissa-09@users.noreply.github.com>
2021-10-05 22:01:01 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,73 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Write a short comment describing this function - Creates a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+    # Initialize symbol for cached inverse of matrix
+    inv <- NULL
+    # Initialize symbol to indicate matrix has changed
+    matChanged <- TRUE
 
+    # Set a matrix in the data structure
+    setMatrix <- function(x) {
+        m <<- x
+        inv <<- NULL
+        matChanged <<- TRUE
+    }
+
+    # Get the currently set matrix
+    getMatrix <- function() m
+
+    # Set the calculated inverse of a matrix, 
+    # and indicate that the matrix is not changed
+    setMatInverse <- function(inverse) {
+        matChanged <<- FALSE
+        inv <<- inverse
+    }
+
+    # Get the inverse of the matrix
+    getMatInverse <- function() inv
+
+    # Returns whether the matrix has changed or not
+    isMatChanged <- function() matChanged
+
+    # Return the list of functions that can be executed
+    # on this data structure
+    list(setMatrix = setMatrix, 
+         getMatrix = getMatrix,
+         setMatInverse = setMatInverse,
+         getMatInverse = getMatInverse,
+         isMatChanged = isMatChanged)
 }
 
 
-## Write a short comment describing this function
+## Write a short comment describing this function - Compute the inverse of the special matrix returned by "makeCacheMatrix"
+## above. If the inverse has already been calculated (and the matrix has not
+## changed), then the "cachesolve" should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+         # Get the cached inverse of matrix
+    inv <- specialMatrix$getMatInverse()
+    # Ask whether the matrix has changed or not
+    isMChanged <- specialMatrix$isMatChanged()
+
+    # If the cached inverse is not NULL and
+    # if the matrix has not changed
+    if(!is.null(inv) && !isMChanged) {
+        message("Get cached inverse of matrix")
+        return(inv)
+    }
+
+    # Get the current matrix for which inverse 
+    # has been requested
+    m <- specialMatrix$getMatrix()
+
+    # Calculate the inverse of matrix
+    inv <- solve(m, ...)
+    # Set the calculated inverse in data structure
+    specialMatrix$setMatInverse(inv)
+
+    message("Get calculated inverse of matrix")
+    inv
 }
